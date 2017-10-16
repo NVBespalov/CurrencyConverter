eb4467863191a868bb0abc8223dadbf2fce4b940
Nick Bespalov <nbespalov@me.com>
2017-10-17 01:25:23 +0500

    fetch rates

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,32 +1,29 @@
 /**
  * Created by nbespalov on 11.04.2017.
  */
-import { createStore, applyMiddleware, compose } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
-import { loadState, saveState } from './localStorage';
+import { createStore, applyMiddleware, compose } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import rootReducer from '../reducers'
 
 export default function configureStore(state) {
-  const initialState = state || loadState() || {};
+  const initialState = state || {}
   const composeEnhancers =
     typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose;
+    }) : compose
 
-  const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+  const enhancer = composeEnhancers(applyMiddleware(thunk, logger))
 
-  const store = createStore(rootReducer, initialState, enhancer);
+  const store = createStore(rootReducer, initialState, enhancer)
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
-      const nextRootReducer = rootReducer;
-      store.replaceReducer(nextRootReducer);
-    });
+      const nextRootReducer = rootReducer
+      store.replaceReducer(nextRootReducer)
+    })
   }
 
-  store.subscribe(() => saveState(store.getState()));
-
-  return store;
+  return store
 }
