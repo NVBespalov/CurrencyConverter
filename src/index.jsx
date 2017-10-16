41a046e2df5ca11c32ed5464d815f413b110b91d
Nick Bespalov <nbespalov@me.com>
2017-10-16 23:57:38 +0500

    initial

---
+++ src/index.jsx
@@ -0,0 +1,37 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import { HashRouter as Router, Route, withRouter } from 'react-router-dom'
+import { Provider, connect } from 'react-redux'
+import { asyncComponent } from 'react-async-component'
+import 'roboto-fontface/css/roboto/sass/roboto-fontface-black-italic.scss'
+import configureStore from './store/configureStore'
+import './styles/general.scss'
+
+const store = configureStore()
+
+const ExchangeContainer = asyncComponent({
+  resolve: () => System.import('containers/Exchange'),
+  LoadingComponent: () => <PageIsLoading />,
+  ErrorComponent: ({ error }) => <PageLoadError error={error} />
+})
+
+
+const Public = withRouter(connect(s => s)(({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (<Component {...props} />)}
+  />
+)))
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+        <div className="App">
+          <Public exact path="/" component={ExchangeContainer} />
+        </div>
+    </Router>
+  </Provider>
+)
+
+ReactDOM.render(<App />, document.querySelector('#root'))
+
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by nbespalov on 11.04.2017.
+ */
+import { combineReducers } from 'redux'
+
+const appReducer = combineReducers({
+
+})
+
+
+export default appReducer
+
