73e5515de061a6b1cd865058e52494816e3a8d97
Nick Bespalov <nbespalov@me.com>
2017-10-21 23:47:04 +0500

    components filling

--- src/components/ExchangeTo/EchangeTo.jsx
+++ src/components/ExchangeTo/EchangeTo.jsx
@@ -1,10 +1,20 @@
 import React, { PureComponent } from 'react'
+import cx from 'classnames'
+import baseStyles from '../../styles/base.styl'
+import styles from './ExchangeTo.styl'
 
 export default class extends PureComponent {
   render() {
     return (
-      <div className='exchangeTo'>
-        EchangeTo.jsx
+      <div className={baseStyles.container}>
+        <div className={styles.leftColumn}>
+          <div className={cx(baseStyles.column, baseStyles.center)}>
+            <span>EUR</span>
+            <span>You Have e100</span></div>
+        </div>
+        <div className={styles.rightColumn}>
+          <span />
+        </div>
       </div>
     )
   }
